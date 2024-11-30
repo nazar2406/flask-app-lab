5edf964a0f7717c39b20893ab8cef473c6ebfd7a
nazar2406 <chukhray24@gmail.com>
2024-11-30 22:13:07 +0200

    done lab-6

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
 def create_app(config_name='config'):
+    # Ініціалізація Flask-додатка
     app = Flask(__name__)
-    app.config.from_object(config_name) #налаштування з файлу config.py
+
+    # Завантаження конфігурації
+    app.config.from_object(config_name)  # конфігурація з config.py
+
+    # Реєстрація блюпринтів у контексті додатка
     with app.app_context():
-        from . import views
-        from .posts import post_bp
-        from .users import user_bp
-        app.register_blueprint(post_bp)
-        app.register_blueprint(user_bp)
-        return app
+        from . import views  # імпорт основних представлень
+        from .posts import post_bp  # імпорт блупринта постів
+        from .users import user_bp  # імпорт блупринта користувачів
+        app.register_blueprint(post_bp)  # реєстрація блюпринта постів
+        app.register_blueprint(user_bp)  # реєстрація блюпринта користувачів
+
+    return app
